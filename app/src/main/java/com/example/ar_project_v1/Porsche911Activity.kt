05d8c4c86fb9df7416b582d819c20b4f88924fe1
MustafaIncee <mustafaince538@gmail.com>
2023-11-12 19:41:12 +0300

    Nissan ve Porsche ve kamera activityleri eklendi

---
+++ app/src/main/java/com/example/ar_project_v1/Porsche911Activity.kt
@@ -0,0 +1,23 @@
+package com.example.ar_project_v1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Porsche911Activity : AppCompatActivity() {
+
+    private lateinit var porscheButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_porsche911)
+
+        porscheButton = findViewById(R.id.porscheButton)
+
+        porscheButton.setOnClickListener {
+            val intent = Intent(this, Porsche911CameraActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
