57d7bf40c6886b99003da9493e544755c0c34491
Mehdi <mehdi.can.akbaba@gmail.com>
2023-11-12 10:07:54 +0300

    Çeşitli eklemeler yapıldı ve spinner içerisindeki seçili olan marka ve modele göre o marka ve  modele ait activity'e geçiş yapıldı. Spinner içerisinden seçili ToyotaCorollaActivity 'ye geçiş sağlandı.

--- app/src/main/java/com/example/ar_project_v1/ScreenActivity.kt
+++ app/src/main/java/com/example/ar_project_v1/ScreenActivity.kt
@@ -9,13 +9,11 @@ import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
-
 class ScreenActivity : AppCompatActivity() {
     private lateinit var markaModelMap: MutableMap<String, MutableList<String>>
     private lateinit var spinnerMarka: Spinner
     private lateinit var spinnerModel: Spinner
     private lateinit var goruntuleButton: Button
-    private lateinit var bilgiButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,33 +22,42 @@ class ScreenActivity : AppCompatActivity() {
         spinnerMarka = findViewById(R.id.marka)
         spinnerModel = findViewById(R.id.model)
         goruntuleButton = findViewById(R.id.goruntule)
-        bilgiButton = findViewById(R.id.bilgi)
 
         initializeMarkaModelMap()
         setupMarkaSpinner()
 
         goruntuleButton.setOnClickListener {
-            val intent = Intent(this@ScreenActivity, MainActivity::class.java)
-            startActivity(intent)
+            val selectedMarka = spinnerMarka.selectedItem as String
+            val selectedModel = spinnerModel.selectedItem as String
+
+            val activityName = "com.example.ar_project_v1.${selectedMarka}${selectedModel}Activity"
+
+            try {
+                val activityClass = Class.forName(activityName)
+                val intent = Intent(this@ScreenActivity, activityClass)
+                startActivity(intent)
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+                // Eğer aktivite bulunamazsa veya oluşturulamazsa buraya düşer
+            }
         }
     }
 
     private fun initializeMarkaModelMap() {
         markaModelMap = mutableMapOf()
 
-        val toyotaModels = mutableListOf("Corolla", "Yaris", "RAV4")
-        val volkswagenModels = mutableListOf("Golf", "Passat", "Tiguan")
-        val daciaModels = mutableListOf("Sandero", "Duster", "Logan")
-        val opelModels = mutableListOf("Corsa", "Astra", "Crossland")
+        val toyotaModels = mutableListOf("Corolla", "Supra")
+        val volkswagenModels = mutableListOf("Polo", "Amarok")
+
 
         markaModelMap["Toyota"] = toyotaModels
         markaModelMap["Volkswagen"] = volkswagenModels
-        markaModelMap["Dacia"] = daciaModels
-        markaModelMap["Opel"] = opelModels
+
     }
 
     private fun setupMarkaSpinner() {
-        val markaAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ArrayList(markaModelMap.keys))
+        val markaAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, ArrayList(markaModelMap.keys))
         markaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerMarka.adapter = markaAdapter
 
@@ -65,7 +72,8 @@ class ScreenActivity : AppCompatActivity() {
     }
 
     private fun setupModelSpinner(marka: String) {
-        val modelAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, markaModelMap.getValue(marka))
+        val modelAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, markaModelMap.getValue(marka))
         modelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerModel.adapter = modelAdapter
     }
